dbc6fea0bd8f04c23453731af64844423c1a5ed4
wale ayandiran <waleayandiran@wales-MacBook-Pro.local>
2018-08-06 17:15:04 +0100

    Express framework and dependecies added

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
  * This is the entry point of the application, this handles the necessary basic configuration such as
  * router setup, import modules, etc
  */
+
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(logger('combined'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const server = app.listen(process.env.PORT || 9000, () => {
+    console.log(`Listening on port ${server.address().port}`);
+});
+
+export default app;
